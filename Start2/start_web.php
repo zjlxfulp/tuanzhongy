85554c1f23201781da8e70de60930f10db3bac83
fuliping <fulp@yidianling.com>
2018-04-13 23:56:21 +0800

    1

--- Start2/start_web.php
+++ Start2/start_web.php
@@ -17,14 +17,14 @@ use \GatewayWorker\Gateway;
 use \GatewayWorker\BusinessWorker;
 use \Workerman\Autoloader;
 
-require_once __DIR__ . '/../../../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 // WebServer
 $web = new WebServer("http://0.0.0.0:55151");
 // WebServer进程数量
 $web->count = 2;
 // 设置站点根目录
-$web->addRoot('www.your_domain.com', __DIR__ . '/Web');
+$web->addRoot('www.your_domain.com', __DIR__ . '/../Chat/Web');
 
 
 // 如果不是在根目录启动，则运行runAll方法
