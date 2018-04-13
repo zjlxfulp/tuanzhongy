d165c4b867a4e5e2c92e7b0cdf952d430c951cba
fuliping <fulp@yidianling.com>
2018-04-13 18:53:18 +0800

    1

--- Applications/Systems/Core/Redis.php
+++ Applications/Systems/Core/Redis.php
@@ -12,12 +12,12 @@ class Redis extends \Redis{
      * 静态工厂方法，返还此类的唯一实例
      */
     public static function getInstance($db = 'db',$select = 0) {
-        $config = \Common\Config\Redis::config($db);
-        $select = empty($select) ? $config['db'] : $select ;
-        self::$_instance = new self();
-        self::$_instance->pconnect($config['host'],$config['port']);
-        self::$_instance->select($select);
-        return self::$_instance;
+//        $config = \Common\Config\Redis::config($db);
+//        $select = empty($select) ? $config['db'] : $select ;
+//        self::$_instance = new self();
+//        self::$_instance->pconnect($config['host'],$config['port']);
+//        self::$_instance->select($select);
+//        return self::$_instance;
     }
 
 }
